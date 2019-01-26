768cf0ed8ef4ccdc2ad2ccbb8c35004fcfd70b16
Jeff Spahr <jeffspahr@users.noreply.github.com>
2019-01-26 14:37:40 -0500

    now prints json logs

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # logger
 go app that logs
 
+Set up environment:
+
+`go get github.com/sirupsen/logrus`
+
 Build the binary:
 
 `env GOOS=linux GOARCH=amd64 go build -v`
@@ -13,3 +17,7 @@ oc new-project logger
 oc new-build https://github.com/jeffspahr/logger
 oc new-app --image-stream=logger
 ```
+
+Breadcrumbs:
+http://callistaenterprise.se/blogg/teknik/2017/08/02/go-blog-series-part10/
+https://github.com/sirupsen/logrus
--- main.go
+++ main.go
@@ -1,11 +1,32 @@
 package main
 
-import "fmt"
 import "time"
+import logrus "github.com/sirupsen/logrus"
+import "flag"
+
+func init() {
+	profile := flag.String("profile", "test", "Environment profile")
+
+	if *profile == "dev" {
+		logrus.SetFormatter(&logrus.TextFormatter{
+			TimestampFormat: "2006-01-02T15:04:05.000",
+			FullTimestamp:   true,
+		})
+	} else {
+		logrus.SetFormatter(&logrus.JSONFormatter{
+			// FieldMap: FieldMap{
+			// 	FieldKeyTime:  "@timestamp",
+			// 	FieldKeyLevel: "@level",
+			// 	FieldKeyMsg:   "@message",
+			// 	FieldKeyFunc:  "@caller",
+			// },
+		})
+	}
+}
 
 func main() {
-  for {
-    fmt.Println("I love logs")
-    time.Sleep(1 * time.Second)
-  }
+	for {
+		logrus.Infof("I love logs")
+		time.Sleep(1 * time.Second)
+	}
 }
