99c4fd0d3bf1a3ca00d7d04cb5c9261ddc14cced
Jeff Moore <jeoffmore@gmail.com>
2019-01-26 17:42:16 -0500

    Added a fix for FieldMap issues in main.go

--- main.go
+++ main.go
@@ -4,27 +4,32 @@ import "time"
 import logrus "github.com/sirupsen/logrus"
 import "flag"
 
-func init() {
+func initFlags() {
 	profile := flag.String("profile", "test", "Environment profile")
+	flag.Parse()
+
+	textFormat := logrus.TextFormatter{
+		TimestampFormat: "2006-01-02T15:04:05.000",
+		FullTimestamp:   true,
+	}
+	jsonFormat := logrus.JSONFormatter{
+		FieldMap: logrus.FieldMap{
+			logrus.FieldKeyTime:  "@timestamp",
+			logrus.FieldKeyLevel: "@level",
+			logrus.FieldKeyMsg:   "@message",
+			logrus.FieldKeyFunc:  "@caller",
+		},
+	}
 
 	if *profile == "dev" {
-		logrus.SetFormatter(&logrus.TextFormatter{
-			TimestampFormat: "2006-01-02T15:04:05.000",
-			FullTimestamp:   true,
-		})
+		logrus.SetFormatter(&textFormat)
 	} else {
-		logrus.SetFormatter(&logrus.JSONFormatter{
-			// FieldMap: FieldMap{
-			// 	FieldKeyTime:  "@timestamp",
-			// 	FieldKeyLevel: "@level",
-			// 	FieldKeyMsg:   "@message",
-			// 	FieldKeyFunc:  "@caller",
-			// },
-		})
+		logrus.SetFormatter(&jsonFormat)
 	}
 }
 
 func main() {
+	initFlags()
 	for {
 		logrus.Infof("I love logs")
 		time.Sleep(1 * time.Second)
